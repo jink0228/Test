a45f35165001d7151f1ae11167a315941819dc60
JinMinu <s9430939@naver.com>
2024-07-27 16:17:24 +0900

    로그인 누르면 이동,css수정

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import Login from "./pages/Login";
+import Welcome from "./Welcom";
 
 function App() {
   const action = useNavigationType();
@@ -46,6 +47,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/welcome" element={<Welcome />} />
     </Routes>
   );
 }
---
+++ front/src/Welcom.js
@@ -0,0 +1,9 @@
+function Welcome() {
+    return (
+        <div>
+            <h1>로그인 완료</h1>
+        </div>
+    );
+}
+
+export default Welcome;
