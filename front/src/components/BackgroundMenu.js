0c9fa19f7d5c812c42e59b138d16556e7af7a199
JinMinu <s9430939@naver.com>
2024-07-27 14:59:29 +0900

    user 아이디, 비번 입력받을 수 있도록

--- front/src/components/BackgroundMenu.js
+++ front/src/components/BackgroundMenu.js
@@ -1,25 +1,46 @@
 import PropTypes from "prop-types";
 import styles from "./BackgroundMenu.module.css";
+import {useState} from "react";
 
 const BackgroundMenu = ({ className = "" }) => {
+
+  const [userID, setuserID] = useState("");
+  const [userPW, setuserPW] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("ID : ", userID);
+    console.log("PW : ", userPW);
+    //여기서 DB로 데이터 보내는 등의 나머지 처리함
+  }
+
   return (
-    <div className={[styles.backgroundMenu, className].join(" ")}>
-      <div className={styles.backgroundMenuChild} />
-      <div className={styles.credentials}>
-        <img
-          className={styles.unionIcon}
-          loading="lazy"
-          alt=""
-          src="/union.svg"
-        />
-        <div className={styles.div}>아이디</div>
-        <div className={styles.div1}>비밀번호</div>
+    <form onSubmit={handleSubmit}>
+      <div className={[styles.backgroundMenu, className].join(" ")}>
+        <div className={styles.backgroundMenuChild} />
+        <div className={styles.credentials}>
+          <input
+            className={styles.unionIconID}
+            placeholder="아이디"
+            type="text"
+            value={userID}
+            onChange={(e) => setuserID(e.target.value)}>
+          </input>
+          <br></br>
+          <input
+            className={styles.unionIconPW}
+            placeholder="비밀번호"
+            type="password"
+            value={userPW}
+            onChange={(e) => setuserPW(e.target.value)}>
+          </input>
       </div>
-      <button className={styles.loginAction}>
+      <button type="submit" className={styles.loginAction}>
         <div className={styles.loginActionChild} />
         <b className={styles.b}>로그인</b>
       </button>
     </div>
+    </form>
   );
 };
 
