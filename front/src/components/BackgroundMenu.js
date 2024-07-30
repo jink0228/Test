d5826af305b22afa55ca1365c89d7c42077b8e4e
JinMinu <s9430939@naver.com>
2024-07-30 14:38:23 +0900

    회원가입 페이지로 이동하는 부분 인터페이스만 짬

--- front/src/components/BackgroundMenu.js
+++ front/src/components/BackgroundMenu.js
@@ -4,7 +4,6 @@ import {useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 const BackgroundMenu = ({ className = "" }) => {
-
   const [userID, setuserID] = useState("");
   const [userPW, setuserPW] = useState("");
   const navigate = useNavigate();
