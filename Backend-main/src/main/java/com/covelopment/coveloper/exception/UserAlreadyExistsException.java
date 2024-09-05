1cd7b06520f22960c44ba526d4316df6966d2e21
Jin Minu <s9430939@naver.com>
2024-09-06 00:00:22 +0900

    백엔드코드 삭제

--- Backend-main/src/main/java/com/covelopment/coveloper/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.covelopment.coveloper.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
